db0579adeb1e110f87c151e8666f2242a2f8b5dc
Qelifern <Qelifern@users.noreply.github.com>
2019-03-28 23:44:56 +0100

    refactoring

--- src/main/java/ironfurnaces/Main.java
+++ src/main/java/ironfurnaces/Main.java
@@ -34,7 +34,6 @@ public class Main
 {
     public static final String MOD_ID = "ironfurnaces";
     public static final String VERSION = "121";
-    public static final String VERSION_STRING = "1.2.1";
     public static final String MC_VERSION = "1.13.2";
 
     public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
